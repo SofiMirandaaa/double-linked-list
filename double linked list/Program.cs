78be39dd596953fc5b4013d65049630cacea89e6
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-18 16:58:45 +0700

    add void addNote

--- double linked list/Program.cs
+++ double linked list/Program.cs
@@ -21,5 +21,23 @@ namespace double_linked_list
     class DoubleLinkedList
     {
         Node START;
+
+        //constructor
+
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            
+
+        }
     }
 }
