509f50d8d836f12cc14d9a16239f908681340fb5
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-18 17:23:25 +0700

    add public bool Search

--- double linked list/Program.cs
+++ double linked list/Program.cs
@@ -75,6 +75,20 @@ namespace double_linked_list
                 previous.next = newNode;
                 return;
             }
+            current.prev = newNode;
+            previous.next = newNode;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            previous = current = START;
+            while (current != null &&
+                rollNo != current.noMhs)
+            {
+                previous = current;
+                current = current.next;
+            }
+            return (current != null);
         }
     }
 }
