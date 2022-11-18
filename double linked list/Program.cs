c5ba66e56eb487e10cd40c32edebf8abeddc6f0f
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2022-11-18 16:52:35 +0700

    add comment, public int, public string

--- double linked list/Program.cs
+++ double linked list/Program.cs
@@ -5,6 +5,16 @@ namespace double_linked_list
     class Node
     {
 
+        /* Node class represent the node of doubly linked list
+         * It consists of the information part and links to
+         * Its succeding and preceeding
+         * In terms of the next and previous */
 
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the preceeding node
+        public Node prev;
     }
 }
